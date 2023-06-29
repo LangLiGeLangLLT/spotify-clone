fa8487fbe35ef514b58c3beaaa714a763087609b
llt <418797644@qq.com>
2023-06-30 00:37:18 +0800

    update

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,28 @@
-function Page() {
-  return <div>Page</div>
+import getSongsByTitle from '@/actions/getSongsByTitle'
+import Header from '@/components/Header'
+import SearchContent from '@/components/SearchContent'
+import SearchInput from '@/components/SearchInput'
+
+interface PageProps {
+  searchParams: {
+    title: string
+  }
+}
+
+async function Page({ searchParams }: PageProps) {
+  const songs = await getSongsByTitle(searchParams.title)
+
+  return (
+    <div className="bg-neutral-900 rounded-lg w-full h-full overflow-hidden overflow-y-auto">
+      <Header className="bg-neutral-900">
+        <div className="mb-2 flex flex-col gap-y-6">
+          <h1 className="text-white text-3xl font-semibold">Search</h1>
+          <SearchInput />
+        </div>
+      </Header>
+      <SearchContent songs={songs} />
+    </div>
+  )
 }
 
 export default Page
