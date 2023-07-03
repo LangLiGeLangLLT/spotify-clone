13b99cfc848657b7ae96bd7b93f393bbed08d9ac
llt <418797644@qq.com>
2023-07-03 23:58:32 +0800

    update player

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -8,6 +8,8 @@ import Box from './Box'
 import SidebarItem from './SidebarItem'
 import Library from './Library'
 import { Song } from '@/types'
+import usePlayer from '@/hooks/usePlayer'
+import { twMerge } from 'tailwind-merge'
 
 interface SideBarProps {
   children: React.ReactNode
@@ -16,6 +18,7 @@ interface SideBarProps {
 
 function SideBar({ children, songs }: SideBarProps) {
   const pathname = usePathname()
+  const player = usePlayer()
   const routes = useMemo(
     () => [
       {
@@ -35,7 +38,7 @@ function SideBar({ children, songs }: SideBarProps) {
   )
 
   return (
-    <div className="flex h-full">
+    <div className={twMerge("flex h-full", player.activeId && "h-[calc(100% - 80px)]")}>
       <aside className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
         <Box>
           <div className="flex flex-col gap-y-4 px-5 py-4">
